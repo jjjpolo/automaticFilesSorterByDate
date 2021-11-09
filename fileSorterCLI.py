0e61691d9136b0caec986587cc5b4af2e3fd745b
Jose Juan Jaramillo Polo <josejuan.jaramillopolo@gmail.com>
2021-11-09 00:48:15 -0600

    [#7]Adding recursive capabilities for folders processing.

--- fileSorterCLI.py
+++ fileSorterCLI.py
@@ -4,8 +4,15 @@ from datetime import datetime   # To handle datetime
 import platform                 # To identify the operative system used
 import shutil                   # To copy, move, and rename files
 import argparse                 # to handle arguments from the command line.
+import time
+import threading
 
 version = "v1.1.0"
+numberOfFilesProcessed = 0
+mainProcessIsRunning = False
+numberOfFilesToProcess = 0
+progress = 0 
+mutex = threading.Lock()
 
 def convert_date(timestamp):
     """Receives a date in epoch format and returns a string 
@@ -22,40 +29,157 @@ def convert_date(timestamp):
         Date in a human-readable format.
     """
     d = datetime.utcfromtimestamp(timestamp)
-    formated_date = d.strftime('%Y-%m-%d')
+    formated_date = d.strftime('%Y-%m-%d') #YYYY-MM-DD
     return formated_date
 
-def createFolder(onWorkPath):
-    if(False == os.path.exists(onWorkPath)):
+def createFolder(newDirPath):
+    """Creates a new folder in the path specified
+
+    Parameters
+    ----------
+    newDirPath : str
+        Full directory path to be created.
+    """
+    if(False == os.path.exists(newDirPath)):
         os.umask(0)
-        os.makedirs(onWorkPath, 7777)
+        os.makedirs(newDirPath, 7777)
 
 def sortFile(sourcePath, file, destinyPath, keepOriginalFile = True):
+    """Sorts a file based on its last modification date properties.
+    If keepOriginalFile is True, the file will be copied to the, if keepOriginalFile
+    is False, the file will moved to the destination directory.
+
+    Parameters
+    ----------
+    sourcePath : str
+        Directory path where the file to be sorted is located.
+    file : str
+        Name of the file to be sorted. 
+        (This will be appended to the sourcePath to create the full path)
+    destinyPath : str
+        Directory path where the sorted file will be either copied or moved.
+    keepOriginalFile : bool, optional
+        If this set to True, the original file will be copied, otherwise it will be moved.
+
+    Returns
+    -------
+    str
+        Date in a human-readable format.
+    """
     sourceFilePath = os.path.join(sourcePath, file.name)
-    if os.path.isdir(sourceFilePath): #if the current analyzed file is a directory skips it because we are only looking for files || FYI shutil copy can not copy folders, instead use copytree
-        print("\n" +  str(file.name) + " is a folder, the software will skip it.")
+    if os.path.isdir(sourceFilePath): 
+        print("\n[sortFile] " +  str(file.name) + " is a folder, the software will skip it.")
+        return False
     elif ("fileSorter" in file.name):
-        print("\n " + str(file.name) + " seems to be part of fileSorter software, the software will skip it.")
+        print("\n[sortFile] " + str(file.name) + " seems to be part of fileSorter software, the software will skip it.")
+        return False
     else:
-        fileProperties = file.stat()#get the properties file
-        lastModificationDate = convert_date(fileProperties.st_mtime) #get Last Modification date and convert it from epoch to human format
-        destinyFileFolderPath = os.path.join(destinyPath, lastModificationDate)
-        
-        print("\n" + sourceFilePath + " || " + lastModificationDate  + "  -->  " + destinyFileFolderPath)
-        
+        fileProperties = file.stat() # Getting file's properties. 
+        lastModificationDate = convert_date(fileProperties.st_mtime) # Getting Last Modification date and convert it from epoch to human format.
+        destinyFileFolderPath = os.path.join(destinyPath, lastModificationDate) # Creating destiny folder path based on last modification date. 
         createFolder(destinyFileFolderPath)
+        print("\n[sortFile] " + sourceFilePath + " || " + lastModificationDate  + "  -->  " + destinyFileFolderPath)
+        
         if(False == os.path.exists(destinyFileFolderPath + "\\" +file.name)):
             if(keepOriginalFile):
-                shutil.copy2(sourceFilePath, destinyFileFolderPath) # FYI shutil copy can not copy folders, instead use copytree
+                shutil.copy2(sourceFilePath, destinyFileFolderPath) # FYI shutil copy can't copy folders, instead use copytree
             else:
                 shutil.move(sourceFilePath, destinyFileFolderPath)
-
+            return True
         else:
-            print("There is a file with the same name on the destiny folder")
+            print("[sortFile] There is a file with the same name on the destiny folder.")
+            return False
 
-def doTheJob(sourcePath, destinationPath, numberOfFiles = -1, keepOriginalFile = True):
-    """Scans all the files in the source directory and passes them as a parameter to the 
-    sortFile function to be sorted based on its last modification property.
+def releaseMutex(functionName = "Undefined", logging = False): 
+    """Tries to release the global mutex to protect shared variables among
+    mainProcess and updateAndDisplayProgress.
+    In case it is not possible to release the mutex it handles the exception
+    so that the software can continue.
+
+    Parameters
+    ----------
+    functionName : str
+        The name of the function that is trying to release the mutex.
+        This is mainly for logging purposes.
+    logging : bool, optional
+        Logging enable.
+    
+    Returns
+    -------
+    bool
+        True it everything goes well and False in cases an exception shows up.
+    """
+    global mutex
+    if(logging):
+        try:
+            print("[{0}] About to release mutex.".format(functionName))
+            mutex.release()
+        except:
+            print("[{0}] Unable to release mutex".format(functionName))
+            return False
+        print("[{0}] Mutex released successfully.".format(functionName))
+    else:
+        try:
+            mutex.release()
+        except:
+            return False
+    return True
+
+def acquireMutex(functionName = "Undefined", logging = False): 
+    """Tries to acquire the global mutex to protect shared variables among
+    mainProcess and updateAndDisplayProgress.
+    In case it is not possible to acquire the mutex it handles the exception
+    so that the software can continue.
+
+    Parameters
+    ----------
+    functionName : str
+        The name of the function that is trying to acquire the mutex.
+        This is mainly for logging purposes.
+    logging : bool, optional
+        Logging enable.
+    
+    Returns
+    -------
+    bool
+        True it everything goes well and False in cases an exception shows up.
+    """
+    global mutex
+    if(logging):
+        try:
+            print("[{0}] About to acquire mutex.".format(functionName))
+            mutex.acquire()
+        except:
+            print ("[{0}] Unable to acquire mutex".format(functionName))
+            return False
+        print("[{0}] Mutex acquired successfully.".format(functionName))
+    else:
+        try:
+            mutex.acquire()
+        except:
+            return False
+    return True
+
+def updateAndDisplayProgress():
+    """Run as a thread, this function do the maths to update the progress of 
+    the process and prints its in the standard output. 
+    """
+    global progress, mutex
+    keepUpdatingProgress =  True #This var will tack mainProcessIsRunning and will be updated only after mutex.acquired()
+    time.sleep(0.15) #Waiting for global variables to be set for the 1st time.
+    while keepUpdatingProgress:
+        acquireMutex("updateAndDisplayProgress")
+        keepUpdatingProgress = mainProcessIsRunning   #Updating its value only when mutex.acquire() is called.
+        progress = (numberOfFilesProcessed * 100) / numberOfFilesToProcess
+        print("[updateAndDisplayProgress] progress: {0}% numberOfFilesProcessed: {1} numberOfFilesToProcess: {2} mainProcessIsRunning: {3}".format(progress, numberOfFilesProcessed, numberOfFilesToProcess, keepUpdatingProgress))
+        releaseMutex("updateAndDisplayProgress")
+        time.sleep(0.1)
+    print("[updateAndDisplayProgress] progress: 100%")
+
+def mainProcess(sourcePath, destinationPath, keepOriginalFile = True):
+    """Scans recursively all the files in the source directory an subdirectories, 
+    and passes them as a parameter to the sortFile function to be sorted based on
+    its last modification date property.
 
     Parameters
     ----------
@@ -65,20 +189,82 @@ def doTheJob(sourcePath, destinationPath, numberOfFiles = -1, keepOriginalFile =
         Dir path where the sorted files will be located.
     numberOfFiles : int
         Limit of files to be processed. If it was set to -1 there will be no limit.
-    keepOriginalFile : bool
+    keepOriginalFile : bool, optional
         Boolean flag indicating if the file should be copied or moved when sorting it.   
     """
-    with os.scandir(sourcePath) as currentDirectory:
-        count = 0
-        for path in currentDirectory:
-            if(os.path.isfile(path)):
-                sortFile(sourcePath, path, destinationPath, keepOriginalFile)   
+    global numberOfFilesProcessed, mutex
+    if numberOfFilesProcessed >= numberOfFilesToProcess:
+        return
+    currentDirectory = os.scandir(sourcePath)
+    for path in currentDirectory:
+        acquireMutex("mainProcess")
+        if(os.path.isfile(path)):
+            if numberOfFilesProcessed >= numberOfFilesToProcess:
+                releaseMutex("mainProcess") #Releasing mutex due to end condition. 
+                return
+            if sortFile(sourcePath, path, destinationPath, keepOriginalFile):
+                numberOfFilesProcessed = numberOfFilesProcessed + 1
+            releaseMutex("mainProcess") #Releasing mutex before next for iteration.
+        else:
+            releaseMutex("mainProcess") #Releasing mutex before recursive iteration.
+            mainProcess(os.path.join(sourcePath, path.name),destinationPath, keepOriginalFile)
 
-            count = count + 1
+def numberOfFilesInFolder(dirPath):
+    """Returns the number of files in the given directory (recursively).
 
-            if numberOfFiles is not -1: # This means that there is no limit on the number of files to be processed.
-                if count >= numberOfFiles:
-                    break;
+    Parameters
+    ----------
+    dirPath : str
+        Dir path from where we need the number of files.
+    
+    Returns
+    -------
+    int
+        Number of files in the given directory.
+    """
+    total = 0
+    for root, dirs, files in os.walk(dirPath):
+            total += len(files) 
+    return total
+
+def doTheJob(sourcePath, destinationPath, numberOfFiles = -1, keepOriginalFile = True, updateProgressFunction = None):
+    """This is the main organizer function which calls the needed functions in the right 
+    order, creates thread for the main process and the progress calc and display, and handles
+    the threads starting by passing the corresponding parameters to each one. 
+
+    Parameters
+    ----------
+    sourcePath : str
+        Dir path from where the files will be taken.
+    destinationPath : str
+        Dir path where the sorted files will be located.
+    numberOfFiles : int, optional
+        Limit of files to be processed. If it was set to -1 there will be no limit.
+    keepOriginalFile : bool, optional
+        Boolean flag indicating if the file should be copied or moved when sorting it.   
+    updateProgressFunction : func, optional
+        A function pointer that helps to identify who is going to update the progress (GUI or CLI mode).
+        In CLI mode it is updateAndDisplayProgress(), in GUI mode is updateProgressBar.
+
+    """
+    global numberOfFilesToProcess, numberOfFilesProcessed, mainProcessIsRunning
+    numberOfFilesProcessed = 0
+    numberOfFilesToProcess = numberOfFilesInFolder(sourcePath) if numberOfFiles == -1 else numberOfFiles
+    mainProcessIsRunning = True
+
+    mainThread = threading.Thread(target=mainProcess, args=(sourcePath, destinationPath, keepOriginalFile))
+    mainThread.start()
+    
+    if updateProgressFunction is not None: #Means that doTheJob was called by the CLI mode
+        progressUpdateThread = threading.Thread(target=updateProgressFunction, args=())
+        progressUpdateThread.start()
+    #else  #In GUI the updating progress is handled in fileSorterGUI.py
+
+    mainThread.join()               #Waiting for the mainThread to be done.
+    mainProcessIsRunning = False    #Notifying progressUpdateThread that mainThread is done.
+    if updateProgressFunction is not None:
+        progressUpdateThread.join()
+    print("[doTheJob] Process Done!")
 
 #_________________________________________________ for CLI mode
 
@@ -98,9 +284,9 @@ def argsAreValid(sourcePath, numberOfFiles):
         if os.path.isdir(sourcePath):
             return True
         else:
-            print ("{0} is not a valid sourcePath.".format(sourcePath))
+            print ("[argsAreValid] {0} is not a valid sourcePath.".format(sourcePath))
     else:
-        print ("{0} is not a valid value for numberOfFiles".format(numberOfFiles))
+        print ("[argsAreValid] {0} is not a valid value for numberOfFiles".format(numberOfFiles))
     return False
 
 def displayArgValues(args):
@@ -151,7 +337,7 @@ def main(argv):
     parser.add_argument("-n","--numberOfFiles",
                         metavar="<numberOfFiles>",
                         type=int,
-                        help="Number of files to processed. Skip this for no limit.",
+                        help="(Optional) The number of files to be processed. Skip this for no limit.",
                         default=-1,
                         nargs='?',
                         const=-1)
@@ -160,7 +346,7 @@ def main(argv):
                         type=str, # Based on argparse documentation bool type is not recommended since void str is false. 
                         help="By default this is set to True so files will be sorted as a copy of the originals. \
                             If you want to move files instead of copying them, set this explicitly to False.",
-                        default="True", # Value for when no using this parameter.
+                        default="True", # Default value for when no using this parameter.
                         nargs='?',
                         const="True")  # Default value for when using this parameter with no explicit value typed.
     
@@ -177,12 +363,12 @@ def main(argv):
     confirmation = input("\n\tContinue? Y/n: ")
     if (confirmation.lower() == "n"):
         print("Action cancelled, exiting now...")
-        sys.exit()
+        sys.exit(0)
     
     # Arguments validation (directory path and numberOfFiles to be processed)    
     if argsAreValid(args.sourcePath, args.numberOfFiles):
-        doTheJob(args.sourcePath, args.destinationPath, args.numberOfFiles, False if (args.keepOriginalFiles.lower() == "false") else True) # moveFiles is passed to keepOriginal so they are opposite values.
-        pause = input("\nDONE! \nYou can close this shell now...")
+        doTheJob(args.sourcePath, args.destinationPath, args.numberOfFiles, False if (args.keepOriginalFiles.lower() == "false") else True, updateAndDisplayProgress)
+        sys.exit(0)
     else:
         print("Error due to invalid arguments.")
         sys.exit(1)
